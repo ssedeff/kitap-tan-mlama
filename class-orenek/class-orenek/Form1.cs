47b34df70ce05070183db382f99c7d1f315bc6cc
ssedeff <145463803+ssedeff@users.noreply.github.com>
2023-11-21 10:59:33 +0300

    Add files via upload

---
+++ class-orenek/class-orenek/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace class_orenek
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            kitap kitap1 = new kitap();
+            kitap1.türü = "roman";
+            kitap1.yazarı = "meryem mart";
+            kitap1.adı = "yok musun";
+            kitap1.sayfaSayısı = 502;
+            kitap1.yayınevi = "Ephesus";
+            kitap1.tipi = "ciltli";
+
+            label1.Text = kitap1.adı + " " + kitap1.sayfaSayısı + " " + kitap1.türü + " " + kitap1.yazarı + " " +
+                kitap1.yayınevi + " " + kitap1.tipi;
+        }
+    }
+}
---
+++ class-orenek/class-orenek/kitap.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_orenek
+{
+    internal class kitap
+    {
+        public string türü;
+        public string yazarı;
+        public string adı;
+        public int sayfaSayısı;
+        public string yayınevi;
+        public string tipi;
+    }
+}
